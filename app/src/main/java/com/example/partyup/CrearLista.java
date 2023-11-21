806748a23d657f63185b10e4d6290fbec97ae00d
angel <angelhernandezmn@gmail.com>
2023-11-21 16:49:18 -0600

    awaagga

--- app/src/main/java/com/example/partyup/CrearLista.java
+++ app/src/main/java/com/example/partyup/CrearLista.java
@@ -6,16 +6,15 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Invitado extends AppCompatActivity {
+public class CrearLista extends AppCompatActivity {
 
-    public void goToListaInvitado(View view) {
-        Intent intent = new Intent(this, ListaInvitado.class);
+    public void goToListaHost(View view) {
+        Intent intent = new Intent(this, ListaHost.class);
         startActivity(intent);
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_invitado);
+        setContentView(R.layout.activity_crearlista);
     }
 }
--- app/src/main/java/com/example/partyup/Host.java
+++ app/src/main/java/com/example/partyup/Host.java
@@ -1,10 +1,17 @@
 package com.example.partyup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Host extends AppCompatActivity {
+
+    public void goToCrearLista(View view) {
+        Intent intent = new Intent(this, CrearLista.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
